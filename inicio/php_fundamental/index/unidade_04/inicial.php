efad3ac792f41a7e3949439f4d15b4281b6d183f
gussobrinho <gustavo-ssobrinho@hotmail.com>
2019-05-27 21:57:21 -0300

    Inicio da codificação em PHP

--- inicio/php_fundamental/index/unidade_04/inicial.php
+++ inicio/php_fundamental/index/unidade_04/inicial.php
@@ -4,7 +4,17 @@
         <meta charset="UTF-8">
         <title>Curso PHP FUNDAMENTAL</title>
     </head>
-
+    
     <body>
+        <p><?php
+                /*
+                    Toda variavel em php comeca com $
+                    Pode ser seguido por letras ou underscore (ex: $alo, $_alo2)
+                    Nao pode conter caracter especial ou espaco
+                    Case sensitive faz a diferenca (ex: $Alow != $alow)
+                */
+                echo ("Gustavo dos Santos Sobrinho");
+            ?>
+        </p>
     </body>
 </html>
---
+++ inicio/php_fundamental/index/unidade_05/variaveis.php
@@ -0,0 +1,16 @@
+<?php
+    $_nome = "Gustavo dos Santos Sobrinho";
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP Fundamental</title>
+    </head>
+    
+    <body>
+        <p><?php echo $_nome ?></p>
+        <p><?php echo $_nome ?></p>
+    </body>
+</html>
